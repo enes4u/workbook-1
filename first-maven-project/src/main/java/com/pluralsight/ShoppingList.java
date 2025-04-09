c6885a4ebc615a54c756b7f301a57f9b434eb7fa
enes4u <enesuzunyearup@gmail.com>
2025-04-09 13:05:11 -0400

    updated print out

--- first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -3,7 +3,7 @@ package com.pluralsight;
 public class ShoppingList {
 
         public static void main(String[] args) {
-            System.out.println("fresh bagel");
+            System.out.println("fresh bagel, milk, tomato, potato");
             System.out.println("eggs");
 
 
