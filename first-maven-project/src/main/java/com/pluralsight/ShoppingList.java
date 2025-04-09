7297429aaea7fe8eaf06a770278cdda4291260eb
enes4u <enesuzunyearup@gmail.com>
2025-04-09 11:46:41 -0400

    completed ShoppingList app

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+        public static void main(String[] args) {
+            System.out.println("fresh bagel");
+            System.out.println("eggs");
+
+
+
+
+        }
+
+
+}
